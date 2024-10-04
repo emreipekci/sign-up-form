75410bda52ab2eb80dbd6d6a9dcefe3abd567b1b
emreipekci <153276209+emreipekci@users.noreply.github.com>
2024-10-04 12:06:41 +0200

    link js file

--- project.js
+++ project.js
@@ -5,6 +5,7 @@ function toggleOpacity() {
   document.querySelector(".img1").style.opacity = 0;
   document.querySelector(".img2").style.opacity = 0;
 }
+
 function imageSwap() {
   setTimeout(() => {
     i = (i + 1) % 3;
