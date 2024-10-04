543593d2c17b1e5d7af97c4aabfbcb53d06cd35f
emreipekci <153276209+emreipekci@users.noreply.github.com>
2024-10-04 12:00:13 +0200

    add sidebar images

---
+++ project.js
@@ -0,0 +1,21 @@
+let i = 0;
+let currentImg;
+
+function toggleOpacity() {
+  document.querySelector(".img1").style.opacity = 0;
+  document.querySelector(".img2").style.opacity = 0;
+}
+function imageSwap() {
+  setTimeout(() => {
+    i = (i + 1) % 3;
+    currentImg = document.querySelector(`.img${i}`);
+    currentImg.style.opacity = 1;
+    if (i === 0) {
+      toggleOpacity();
+    }
+    imageSwap();
+  }, 3000);
+}
+
+toggleOpacity();
+imageSwap();
